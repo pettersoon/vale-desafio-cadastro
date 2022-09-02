a7d85956f0db7056cfc19c6664446b3cd8ea894d
vitor.moreira <vitor.moreira@valemobi.com.br>
2022-09-02 14:16:47 -0300

    feat: Adicionado arquivo teste usuario service

--- cadastro-usuario/src/main/java/cadastrousuario/service/UsuarioService.java
+++ cadastro-usuario/src/main/java/cadastrousuario/service/UsuarioService.java
@@ -1,4 +1,39 @@
 package cadastrousuario.service;
 
+import cadastrousuario.Repository.EnderecoRepository;
+import cadastrousuario.Repository.UsuarioRepository;
+import cadastrousuario.model.Endereco;
+import cadastrousuario.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class UsuarioService {
+    
+    @Autowired
+    UsuarioRepository repository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    public String salvarUsuario(Usuario usuario){
+      return "Chamada feita";
+    }
+    
+    public void updateUsuario(Usuario usuario) {
+        repository.save(usuario);
+        repository.atualizarUsuario(
+                usuario.getIdUsuario(),
+                usuario.getNome(),
+                usuario.getEmail(),
+                usuario.getSenha());
+
+    }
+    
 }
+
+    
